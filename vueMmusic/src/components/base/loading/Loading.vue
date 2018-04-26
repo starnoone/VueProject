b2a56dd98129ee185cf29bc1818524b74e90bab7
starnoone <1428001187@qq.com>
2018-04-26 18:04:06 +0800

    6.03

---
+++ vueMmusic/src/components/base/loading/Loading.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="loading" v-show="state">
+		<i class="icon"></i>
+		<span class="text" v-text = "title"></span>
+	</div>
+</template>
+
+<script>
+export default{
+	name:'',
+	data(){
+		return {
+
+		}
+	},
+	methods:{
+
+	},
+	props:{
+		title:{
+			type: String,
+			default:'加载中...'
+		},
+		state:{
+			type: Boolean,
+			default:true
+		}
+	}
+}
+</script>
+
+<style lang='scss' scoped>
+.loading{
+	display:flex;
+	position: fixed;
+	width:160px;
+	line-height: 40px;
+	top:50%;
+	left:50%;
+	margin-left:-80px;
+	justify-content:center;
+	align-items:center;
+	.icon{
+		dispaly:inline-block;
+		width: 15px;
+		height:15px;
+		background: url(loading.gif) no-repeat;
+		background-position: center center;
+		background-size:100% 100%;
+		margin-right:3px;
+	}
+	.text{
+		font-size:14px;
+		color:#333;
+	}
+}
+</style>
--- vueMmusic/src/pages/Index.vue
+++ vueMmusic/src/pages/Index.vue
@@ -1,39 +1,54 @@
 <template>
 	<div class="">
+		<!-- S 头部 -->
 		<HeaderTab></HeaderTab>
-		<div class="slider">
-			<div class="swiper-container">
-			    <div class="swiper-wrapper">
-			        <div class="swiper-slide" v-for="(v,k) in slider">
-			        	<img :src='v.picUrl'>
-			        </div>
-			    </div>
-			    <div class="swiper-pagination"></div>
+		<!-- E 头部 -->
+		<div class="toplist-wrapper">
+			<div class="content">
+				<!-- S 轮播 -->
+				<div class="slider">
+					<div class="swiper-container">
+					    <div class="swiper-wrapper">
+					        <div class="swiper-slide" v-for="(v,k) in slider">
+					        	<img :src='v.picUrl'>
+					        </div>
+					    </div>
+					    <div class="swiper-pagination"></div>
+					</div>
+				</div>
+				<!-- E 轮播 -->
+				<!-- S 歌曲排行榜列表 -->
+				<div class="rank-list">
+					<h2>歌曲排行榜</h2>
+					<div class="song-container">
+						<ul>
+							<li v-for = "(v,k) in songlist">
+								<div class="thumb">
+									<img :src="'https://y.gtimg.cn/music/photo_new/T002R90x90M000'+v.data.albummid+'.jpg?max_age=2592000'">
+								</div>
+								<div class="info">
+									<span class="song-name">{{v.data.songname}}</span>
+									<span class="songer-name">{{v.data.singer[0].name}}</span>
+								</div>
+								<div class="play-time">
+									{{v.data.interval | getTime}}
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- E 歌曲排行榜列表 -->
 			</div>
 		</div>
-		<div class="rank-list">
-			<h2>歌曲排行榜</h2>
-			<div class="song-container">
-				<ul>
-					<li v-for = "(v,k) in songlist">
-						<div class="thumb">
-							
-						</div>
-						<div class="info">
-							<span class="song-name">{{v.data.songname}}</span>
-							<span class="songer-name">{{v.data.singer[0].name}}</span>
-						</div>
-						<div class="play-time"></div>
-					</li>
-				</ul>
-			</div>
-		</div>
+		<Loading :state = "loadingState"></Loading>
 	</div>
 </template>
 
 <script>
 //引入头部组件
 import HeaderTab from '@/components/HeaderTab';
+//引入loading组件
+import Loading from '@/components/base/loading/Loading';
 
 //引入API
 import indexApi from '../api/indexApi';
@@ -44,53 +59,88 @@ import jsonp from 'jsonp';
 //引入swiper插件
 import Swiper from 'swiper';
 import 'swiper/dist/css/swiper.min.css';
+//引入better-scroll
+import BScroll from 'better-scroll';
 
 export default{
 	name:'',
 	data(){
 		return {
 			slider:[],
-			songlist:[]
+			songlist:[],
+			bannerUrl:'',
+			playListUrl:'',
+			myScroll:null,
+			loadingState:true
 		}
 	},
-	methods:{
-
-	},
 	components:{
-		HeaderTab
+		HeaderTab,
+		Loading
+	},
+	filters:{
+		getTime:function(t){
+			var m = parseInt(t/60);
+			var s = t%60;
+			s = s<10 ? ('0'+s) :s;
+			return m + '分' + s;
+		}
 	},
 	mounted:function(){
-		var _self = this;
-		let bannerUrl = indexApi.bannerApi;
-		let playListUrl = indexApi.topListApi;
+		this.bannerUrl = indexApi.bannerApi;
+		//获取播放列表
+		this.getSlider();
 		//获取轮播图片地址
-		jsonp(bannerUrl,{param:'jsonpCallback'},(err,data) => {
-			this.slider = data.data.slider;
-			//使用swiper插件
-			this.$nextTick(function(){
-				let mySwiper = new Swiper('.swiper-container',{
-					pagination: {
-					    el: '.swiper-pagination',
-					    type: 'bullets',
-					    bulletClass:'bullet',
-            			bulletActiveClass:'bullet-active'
-					  }
-				})
-			})
+		this.getTopList();
+		//调用better-scroll
+		this.myScroll = new BScroll('.toplist-wrapper',{
+			scrollY:true,
+			pullUpLoad:{
+				threshold:50
+			}
 		});
-
-		//播放列表
-		jsonp(playListUrl,{param:'jsonpCallback'},(err,data) => {
-			this.songlist = data.songlist;
-			console.log(this.songlist);
-		})
-
+		//上拉加载
+		this.myScroll.on('pullingUp',()=>{
+			this.loadingState = true;
+			this.getTopList();
+			this.myScroll.refresh();
+			this.myScroll.finishPullUp();
+		});
+	},
+	methods:{
+		getTopList:function(){//播放列表
+			this.playListUrl = indexApi.topListApi + this.songlist.length;
+			jsonp(this.playListUrl,{param:'jsonpCallback'},(err,data) => {
+				this.songlist = this.songlist.concat(data.songlist);
+				this.loadingState = false;
+			});
+		},
+		getSlider:function(){//轮播图片地址
+			jsonp(this.bannerUrl,{param:'jsonpCallback'},(err,data) => {
+				this.slider = data.data.slider;
+				//使用swiper插件
+				this.$nextTick(function(){
+					let mySwiper = new Swiper('.swiper-container',{
+						pagination: {
+						    el: '.swiper-pagination',
+						    type: 'bullets',
+						    bulletClass:'bullet',
+	            			bulletActiveClass:'bullet-active'
+						  }
+					})
+				})
+			});
+		}
 	}
 }
 </script>
 
 <style lang='scss'>
 @import '../assets/scss/_themes.scss';
+.toplist-wrapper{
+	height: calc(100vh - 105px);
+	overflow: hidden;
+}
 .slider{
 	.swiper-container{
 		.swiper-wrapper{
@@ -138,6 +188,10 @@ export default{
 					width:60px;
 					height:60px;
 					background-color: pink;
+					img{
+						width:100%;
+						height: 100%;
+					}
 				}
 				.info{
 					width:205px;
@@ -157,7 +211,8 @@ export default{
 				.play-time{
 					width:60px;
 					padding-top:15px;
-					background: #fff;
+					color:$gray;
+					font-size:14px;
 				}
 			}
 		}
